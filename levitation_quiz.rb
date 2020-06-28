658ea199e6e03de5759805d27a2f7bf0532b68e2
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 13:47:04 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,16 +1,14 @@
 
 def levitation_quiz
-  puts "What is the spell that enacts levitation?"
-  answer = gets.chomp
+  
 counter = 1
  
 while counter <= 5
-  puts "The counter is at: #{counter}"
-  break if counter == 3
-  counter = counter + 1
+  puts "What is the spell that enacts levitation?"
+  answer = gets.chomp
+  break if answer == 
 end
- 
-puts "We're done looping!"
+
 end
 
 
