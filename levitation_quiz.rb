3debafa85f8bd5dfb68482e3b6b0d21d89be015a
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 13:47:44 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,13 +1,12 @@
 
 def levitation_quiz
-  
-counter = 1
  
 while counter <= 5
   puts "What is the spell that enacts levitation?"
   answer = gets.chomp
   break if answer == "Wingardium Leviosa"
 end
+puts "You passed the quiz!"
 
 end
 
