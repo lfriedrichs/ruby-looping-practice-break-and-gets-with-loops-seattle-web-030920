f3e7577739198d43f1f97561f9944a27022a0858
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 13:44:38 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,14 @@
 
 def levitation_quiz
-	#your code here
+	#your counter = 1
+ 
+while counter <= 5
+  puts "The counter is at: #{counter}"
+  break if counter == 3
+  counter = counter + 1
+end
+ 
+puts "We're done looping!" here
 end
 
 
